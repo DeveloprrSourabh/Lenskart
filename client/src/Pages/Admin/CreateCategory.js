977c40497935bd71d569484036e73039f41f7923
Sourabh <developrr.sourabh@gmail.com>
2023-11-03 16:21:09 +0530

    Catgory Update And Delete Complete in Client Side

--- client/src/Pages/Admin/CreateCategory.js
+++ client/src/Pages/Admin/CreateCategory.js
@@ -8,12 +8,22 @@ import useCategory from "../../hooks/useCategory";
 const host = "http://localhost:8080";
 
 const CreateCategory = () => {
+  const [editForm, setEditForm] = useState("");
   const [categories, setCategories] = useState([]);
   const [auth, setAuth] = useAuth();
   const [category, setCategory] = useState({
     name: "",
     email: "",
   });
+  const [newCategory, setNewCategory] = useState({
+    name: "",
+  });
+  const onChangeNew = (e) => {
+    setNewCategory({
+      ...newCategory,
+      [e.target.name]: e.target.value,
+    });
+  };
   const onChange = (e) => {
     setCategory({
       ...category,
@@ -68,6 +78,51 @@ const CreateCategory = () => {
     getAllcategory();
   }, []);
 
+  //   Delete Category
+  const handleDelete = async (id) => {
+    try {
+      const res = await fetch(`${host}/api/v1/category/delete-category/${id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: auth?.token,
+        },
+      });
+      const data = await res.json();
+      if (data.success) {
+        toast.success(data.message);
+        getAllcategory();
+      } else {
+        toast.success(data.message);
+      }
+    } catch (error) {
+      toast.error("Something Went Wrong");
+    }
+  };
+
+  //   Update Category
+  const handleUpdate = async (e, id) => {
+    e.preventDefault();
+    try {
+      const res = await fetch(`${host}/api/v1/category/update-category/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: auth?.token,
+        },
+        body: JSON.stringify(newCategory),
+      });
+      const data = await res.json();
+      if (data.success) {
+        toast.success(data.message);
+        getAllcategory();
+      } else {
+        toast.success(data.message);
+      }
+    } catch (error) {
+      toast.error("Something Went Wrong");
+    }
+  };
   return (
     <Layout>
       <div className="admin-dashboard">
@@ -96,28 +151,74 @@ const CreateCategory = () => {
             </div>
           </div>
           <div className="col-sm-4">
-            <div className="all-categories" id="myScroll">
-              {categories.map((c) => (
-                <div className="categogy-card d-flex my-3 justify-content-between">
-                  <div className="cat-name">
-                    <h2>{c.name}</h2>
+            <div className="all-categories " id="myScroll">
+              {categories.map((c) => {
+                return (
+                  <>
+                    <div
+                      id="main-category-card"
+                      key={c._id}
+                      className={`categogy-card d-flex mb-3 me-3 justify-content-between ${
+                        editForm && editForm === c.name ? "active" : ""
+                      }`}
+                    >
+                      <div className="cat-name">
+                        <h2>{c.name}</h2>
+                        <p className="cat-time">
+                          Created By : {auth?.user?.name}
+                        </p>
+                        <p className="cat-time">
+                          Created At : {new Date(c.createdAt).toDateString()}
+                        </p>
+                      </div>
 
-                    <p className="cat-time">Created By : {auth?.user?.name}</p>
-                    <p className="cat-time">
-                      Created At : {new Date(c.createdAt).toDateString()}
-                    </p>
-                  </div>
-
-                  <div className="cat-option">
-                    <span className="del-cat mx-2">
-                      <i class="fa-solid fa-trash"></i>
-                    </span>
-                    <span className="edit-cat mx-2">
-                      <i class="fa-solid fa-pen-to-square"></i>
-                    </span>
-                  </div>
-                </div>
-              ))}
+                      <div className="cat-option">
+                        <span
+                          className="del-cat mx-2"
+                          onClick={() => {
+                            handleDelete(c._id);
+                          }}
+                        >
+                          <i className="fa-solid fa-trash"></i>
+                        </span>
+                        <span
+                          onClick={() => {
+                            setEditForm(c.name);
+                          }}
+                          className="edit-cat mx-2"
+                        >
+                          <i className="fa-solid fa-pen-to-square"></i>
+                        </span>
+                      </div>
+                    </div>
+                    <div class="categogy-card mb-3 me-3">
+                      <form
+                        onSubmit={(e) => {
+                          handleUpdate(e, c._id);
+                        }}
+                        className="d-flex align-items-center  justify-content-between align-align-items-center"
+                      >
+                        <div class="cat-name ">
+                          <div class="edit-input">
+                            <input
+                              onChange={onChangeNew}
+                              name="name"
+                              type="text"
+                              value={newCategory.name}
+                            />
+                          </div>
+                        </div>
+                        <div className="update-category">
+                          <button class="cat-update-btn w mt-1">Update</button>
+                        </div>
+                      </form>
+                      <p className="cat-time mt-0">
+                        Created By : {auth?.user?.name}
+                      </p>
+                    </div>
+                  </>
+                );
+              })}
             </div>
           </div>
         </div>
