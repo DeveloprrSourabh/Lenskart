790973eb6aa0531f92bfac2b8688d6e2d147d90f
Sourabh Sain <114208070+DeveloprrSourabh@users.noreply.github.com>
2023-11-20 16:45:17 +0530

    change app.js

--- client/src/App.js
+++ client/src/App.js
@@ -20,16 +20,9 @@ import UserProfile from "./Pages/User/Profile";
 import { useEffect, useState } from "react";
 
 function App() {
-  const [message, setMessage] = useState("");
-  useEffect(() => {
-    fetch("https://YOUR_BACKEND_URL.com")
-      .then((res) => res.json())
-      .then((data) => setMessage(data.message));
-  }, []);
   return (
     <>
       <Routes>
-        <h1>{message}</h1>
         <Route path="/" element={<Home />} />
         <Route path="/shop" element={<Shop />} />
         <Route path="/shop/:slug" element={<SingleShop />} />
